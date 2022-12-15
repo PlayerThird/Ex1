2ee54887c1aee5ff2525a4fc915026f796691508
PlayerThird030 <verevka.rud@gmail.com>
2022-12-15 18:49:39 +0300

    Initial git

---
+++ HM1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа через Enter");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Yes, yes, YES!");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ является квадратом первого числа");
+}
