77f8a26c22b6f4c7b338ff37e5b9f558864c8fa4
PlayerThird030 <verevka.rud@gmail.com>
2022-12-15 18:59:36 +0300

    Home Work 2

---
+++ HM2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа через Enter");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 > number1)
+{
+    Console.WriteLine("Max " + number2);
+}
+else
+{
+    Console.WriteLine("Max " + number1);
+}
