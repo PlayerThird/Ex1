909d4bc2cf4723361b8f3d9d56c24f3b1f73bc2e
PlayerThird030 <verevka.rud@gmail.com>
2022-12-16 16:56:01 +0300

    Home Work8

---
+++ HM8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число от 1");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < x; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($" " + (i) + " ");
+    }
+}
