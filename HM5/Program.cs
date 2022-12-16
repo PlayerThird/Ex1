48bde18bdc33b300610f01fb9f94c9de3908407e
PlayerThird030 <verevka.rud@gmail.com>
2022-12-16 14:19:48 +0300

    Home Work 5

---
+++ HM5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int nonmber = number * (-1);
+Console.Write(nonmber);
+while (nonmber != number)
+{
+    nonmber++;
+    Console.Write(" ");
+    Console.Write(nonmber);
+}
