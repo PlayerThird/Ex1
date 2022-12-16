4d0681b1d8bea7b8770d176b820d59b41c022f2b
PlayerThird030 <verevka.rud@gmail.com>
2022-12-16 14:04:13 +0300

    Доделал дъявольскую работу с максимальным из 3х чисел

---
+++ HM4/Program.cs
@@ -0,0 +1,26 @@
+﻿int a, b, c;
+string test = Console.ReadLine(); // Переменная для текста с консоли
+string[] splitString = test.Split(' ');// Создали текстовый массив, каждая новая переменная это кусочек текста, отделённый пробелом
+int[] ints = new int[3];// Создали новый массив для 3х чисел
+for (int i = 0; i < 3; i++ )
+{
+int x;
+int.TryParse(splitString[i], out x); //Парсим, т.е. переделываем переменную string в int
+ints[i] = x; // Закидываем результат Парса в массив
+}
+ // Выше сделали так, чтобы можно было ввести 3 числа в строку
+a = ints[0];
+b= ints[1];
+c = ints[2];
+int max = a; 
+Console.WriteLine(a);
+if (max < b)
+{
+    max = b;
+}
+if(max < c)
+{
+    max = c;
+}
+Console.WriteLine("Максимальное число = ");
+Console.WriteLine(max);
