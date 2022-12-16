b5f2af0eb292ad60d93c3256d29b2c3ff2423348
PlayerThird030 <verevka.rud@gmail.com>
2022-12-16 16:40:25 +0300

    Home work 7

---
+++ Experemental7/Program.cs
@@ -0,0 +1,8 @@
+﻿string str = Console.ReadLine();
+string[] parts = str.Split('.');
+Console.WriteLine("Целая часть: " + parts[0]);
+Console.WriteLine("Дробная часть: " + parts[1]);
+//string chislo = parts[1].Substring(parts[1].IndexOf(3));
+int ind = parts[1].Length - 1;
+parts[1] = parts[1].Remove(0, 2);
+Console.WriteLine(parts[1]); 
---
+++ HM7/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите ТОЛЬКО 3-х значное число");
+string str = Console.ReadLine();
+str = str.Remove(0, 2);
+Console.WriteLine(str); 
